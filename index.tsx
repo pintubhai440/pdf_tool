eeba3ef27e74493095cad675b405a6131bd846dd
pintubhai440 <p7431309@gmail.com>
2026-02-18 02:19:25 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,20 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async'; // 👈 यह Import करें
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <StrictMode>
-    {/* 👇 App को HelmetProvider के अंदर रखें */}
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
